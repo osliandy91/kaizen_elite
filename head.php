7071e76805b4e60c85b2a600ae7ade4013637166
osliandy <operez@toools.es>
2025-06-03 18:02:12 +0200

    wip

--- head.php
+++ head.php
@@ -46,6 +46,13 @@
         }
     </script>
 
+    <script>(function(w){w.fpr=w.fpr||function(){w.fpr.q = w.fpr.q||[];w.fpr.q[arguments[0]=='set'?'unshift':'push'](arguments);};})(window);
+        fpr("init", {cid:"okpqrvyz"});
+        fpr("click");
+    </script>
+    <script src="https://cdn.firstpromoter.com/fpr.js" async></script>
+
+
 </head>
 
 <body id="page-top">
